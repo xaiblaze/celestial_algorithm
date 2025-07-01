ed5445a24bd3a43d1a0337fa1cf8ac1eb9bfe8f4
Xavier <xaiblaze@gmail.com>
2025-07-01 16:05:13 -0700

    v2

--- main.py
+++ main.py
@@ -19,13 +19,24 @@ return all dates where both J and S are in the sky at 15:30
 
 from astronomy import *
 from datetime import datetime, timezone, timedelta
+from zoneinfo import ZoneInfo
 
 def isBetween(cur, start, end):
-    return cur >= start and cur <= end
+    if start <= end:
+        return start <= cur <= end
+    else:
+        # The object is up across midnight
+        return cur >= start or cur <= end
 
-def planetDays(iBody: Body) -> set:
+def planetDays(iBody: Body, location: dict, wedding: dict) -> set:
 
     planet_set = set()
+
+    iObserver = Observer(location["lat"], 
+                         location["long"],
+                         location["height"])
+    iDirections = [Direction.Rise, Direction.Set]
+    iLimitDays = 1      
 
     for day in search_days:
         # create target time for iDay
@@ -45,13 +56,17 @@ def planetDays(iBody: Body) -> set:
         iMonth = iDate.month 
         iSecond = 0 # dont care about sec
 
-        wedding_time = datetime(iYear, iMonth, iDay, WEDDING_HOUR,
-                                WEDDING_MIN, iSecond)
-        
-        confirm_day = interval[Direction.Rise]
-        confirm_date = f"{confirm_day.year}/{confirm_day.month}/{confirm_day.day}"
+        wedding_time_pdt = datetime(iYear, iMonth, iDay, wedding["hour"], 
+                                    wedding["min"], tzinfo=location["tz"])
 
-        planet_set.add(confirm_date) if isBetween(wedding_time, interval[Direction.Rise], interval[Direction.Set]) else None
+        local_day = wedding_time_pdt.date()
+        confirm_date = f"{local_day.year}/{local_day.month}/{local_day.day}" 
+
+        if isBetween(wedding_time_pdt, 
+                     interval[Direction.Rise].astimezone(location["tz"]), 
+                     interval[Direction.Set].astimezone(location["tz"])):
+            
+            planet_set.add(confirm_date) 
 
     return planet_set
 
@@ -63,28 +78,20 @@ END_DAY = NEED_DAY + 2*YEAR
 search_days = [Time(x) for x in range(NEED_DAY, END_DAY)]
 
 tz_utc = timezone.utc
-tz_pdt = timezone(timedelta(hours=-7))
-WEDDING_HOUR = 15
-WEDDING_MIN = 30
 
 
-# LOS ANGELES (PDT offset UTC-7)
-lat = 34.0522
-long = 118.2426
-height = 0
+wedding = {"hour": 15, "min": 30}
+
+# LA
+tz_la = ZoneInfo('America/Los_Angeles')
+LA = {"lat":34.0522, "long":-118.2426, "height": 0, "tz":tz_la}
 
 
-iBody = Body.Jupiter
-iObserver = Observer(lat, long, height)
-iDirections = [Direction.Rise, Direction.Set]
-iLimitDays = 1
-
-
-jupiter_set = planetDays(Body.Jupiter)
-#print(sorted(jupiter_set))
+jupiter_set = planetDays(Body.Jupiter,location=LA, wedding=wedding)
+#print("jupiter ,", sorted(jupiter_set))
 print()
-saturn_set = planetDays(Body.Saturn)
-#print(sorted(saturn_set))
+saturn_set = planetDays(Body.Saturn, location=LA, wedding=wedding)
+#print("saturn, ", sorted(saturn_set))
 #print(saturn_set)
 
 jupiter_saturn_set = jupiter_set.intersection(saturn_set)
